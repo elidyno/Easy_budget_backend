0d002749673dc14f8896a37e0a68fca9d65dd498
Eli Dynovisz <edynovis@g.jct.ac.il>
2025-12-04 21:46:28 +0200

    CORSMiddleware added

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from database import engine, Base
 from routers import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 # יצירת טבלאות אם לא קיימות
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 
